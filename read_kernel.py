428b1503375fc82ec39cdf2610f0a8ce2ca1b915
Jason Zhong <jason@meom.moe>
2017-12-02 18:59:12 +0800

    Modify: read_kernel.py
    It now checks mem dump against kernel.bin

--- read_kernel.py
+++ read_kernel.py
@@ -1,9 +1,13 @@
-with open("kernel.bin", "rb") as f:
+with open("C:\\Users\\Jason\\Desktop\\cpu-memory dump\\ext-0000-0860.bin", "rb") as f1, open("kernel.bin", "rb") as f2:
     counter = 0
     print()
-    instr = f.read(4)
-    while instr != b"":
-        print(counter, end="\t")
-        print("{0:08b}".format(instr[3]) + " {0:08b}".format(instr[2]) + " {0:08b}".format(instr[1]) + " {0:08b}".format(instr[0]))
+    mem = f1.read(4)
+    instr = f2.read(4)
+    while instr != b"" and mem != b"":
         counter += 1
-        instr = f.read(4)
+        if (mem[0:1] != instr[0:1]):
+            print(counter, end="\t")
+            print("{0:08b}".format(mem[1]) + " {0:08b}".format(mem[0]) + "; {0:08b}".format(instr[1]) + " {0:08b}".format(instr[0]))
+        
+        mem = f1.read(4)
+        instr = f2.read(4)
