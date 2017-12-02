e66025ae65a05263e1b220b6311f0633f88b3810
jason <j@meom.moe>
2017-12-02 18:45:35 +0800

    Modify: read_kernel.py

--- read_kernel.py
+++ read_kernel.py
@@ -1,10 +1,9 @@
 with open("kernel.bin", "rb") as f:
     counter = 0
     print()
-    instr = f.read(2)
+    instr = f.read(4)
     while instr != b"":
         print(counter, end="\t")
-        print("{0:08b}".format(instr[1]) + " {0:08b}".format(instr[0]))
-        counter += 2
-        f.read(2)
-        instr = f.read(2)
+        print("{0:08b}".format(instr[3]) + " {0:08b}".format(instr[2]) + " {0:08b}".format(instr[1]) + " {0:08b}".format(instr[0]))
+        counter += 1
+        instr = f.read(4)
